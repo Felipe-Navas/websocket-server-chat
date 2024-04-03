d192834a38c393e5824bb0d3a66cdfb7228e2896
Felipe Navas <felipenavas.itec@gmail.com>
2024-04-03 20:30:40 -0300

    Add example env

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,61 +1,58 @@
-const miForm = document.querySelector('form');
+const miForm = document.querySelector('form')
 
-miForm.addEventListener('submit', ev =>{
-    ev.preventDefault();
-    const formData = {};
+miForm.addEventListener('submit', (ev) => {
+  ev.preventDefault()
+  const formData = {}
 
-    for (let el of miForm.elements) {
-        if ( el.name.length > 0 ) {
-            formData[el.name] = el.value;
-        };
-    };
-    
-    fetch('http://localhost:8080/api/auth/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify( formData )
+  for (let el of miForm.elements) {
+    if (el.name.length > 0) {
+      formData[el.name] = el.value
+    }
+  }
+
+  fetch('http://localhost:8080/api/auth/login', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(formData),
+  })
+    .then((resp) => resp.json())
+    .then(({ msg, token }) => {
+      if (msg) {
+        return console.error(msg)
+      }
+
+      // Guardo el token en el localStorage
+      localStorage.setItem('token', token)
+      window.location = 'chat.html'
     })
-        .then( resp => resp.json() )
-        .then( ({ msg, token }) => {
-            if ( msg ) {
-                return console.error( msg );
-            };
-
-            // Guardo el token en el localStorage
-            localStorage.setItem('token', token);
-            window.location = 'chat.html';
-        })
-        .catch( err => {
-            console.log(err);
-        });
-});
-
+    .catch((err) => {
+      console.log(err)
+    })
+})
 
 function handleCredentialResponse(response) {
+  // Google Token : ID_TOKEN
+  const body = { id_token: response.credential }
 
-    // Google Token : ID_TOKEN
-    const body = { id_token: response.credential};
-
-    fetch('http://localhost:8080/api/auth/google', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify( body )
+  fetch('http://localhost:8080/api/auth/google', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(body),
+  })
+    .then((resp) => resp.json())
+    .then(({ token }) => {
+      // Guardo el token en el localStorage
+      localStorage.setItem('token', token)
+      window.location = 'chat.html'
     })
-        .then( resp => resp.json() )
-        .then( ({ token }) => {
-            // Guardo el token en el localStorage
-            localStorage.setItem('token', token);
-            window.location = 'chat.html';
-        })
-        .catch( console.warn )
+    .catch(console.warn)
+}
 
-};
-
-const button = document.getElementById('google_signout');
+const button = document.getElementById('google_signout')
 button.onclick = () => {
-    console.log(google.accounts.id)
-    google.accounts.id.revoke( localStorage.getItem( 'email' ), done => {
-        localStorage.clear();
-        location.reload();
-    });
-};
+  console.log(google.accounts.id)
+  google.accounts.id.revoke(localStorage.getItem('email'), (done) => {
+    localStorage.clear()
+    location.reload()
+  })
+}
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,118 +1,114 @@
-
-let usuarioAutenticado = null;
-let socket = null;
+let usuarioAutenticado = null
+let socket = null
 
 // Referencias html
-const txtUid = document.querySelector('#txtUid');
-const txtMensaje = document.querySelector('#txtMensaje');
-const ulUsuarios = document.querySelector('#ulUsuarios');
-const ulMensajes = document.querySelector('#ulMensajes');
-const btnSalir = document.querySelector('#btnSalir');
-
+const txtUid = document.querySelector('#txtUid')
+const txtMensaje = document.querySelector('#txtMensaje')
+const ulUsuarios = document.querySelector('#ulUsuarios')
+const ulMensajes = document.querySelector('#ulMensajes')
+const btnSalir = document.querySelector('#btnSalir')
 
 // Validar el token del localstorage
 const validarJWT = async () => {
-    const token = localStorage.getItem('token') || '';
+  const token = localStorage.getItem('token') || ''
 
-    if (token.length <= 10) {
-        window.location = 'index.html';
-        throw new Error('No hay token en el servidor');
-    };
+  if (token.length <= 10) {
+    window.location = 'index.html'
+    throw new Error('No hay token en el servidor')
+  }
 
-    const resp = await fetch('http://localhost:8080/api/auth/',
-        {
-            headers: { 'x-token': token }
-        });
+  const resp = await fetch('http://localhost:8080/api/auth/', {
+    headers: { 'x-token': token },
+  })
 
-    const { usuarioAutenticado: userDB, token: tokenDB } = await resp.json();
+  const { usuarioAutenticado: userDB, token: tokenDB } = await resp.json()
 
-    // Restauro el JWT
-    localStorage.setItem('token', tokenDB);
-    usuarioAutenticado = userDB;
+  // Restauro el JWT
+  localStorage.setItem('token', tokenDB)
+  usuarioAutenticado = userDB
 
-    document.title = usuarioAutenticado.nombre;
+  document.title = usuarioAutenticado.nombre
 
-    await conectarSocket();
-};
+  await conectarSocket()
+}
 
 const conectarSocket = async () => {
-    socket = io({
-        'extraHeaders': {
-            'x-token': localStorage.getItem('token')
-        }
-    });
+  socket = io({
+    extraHeaders: {
+      'x-token': localStorage.getItem('token'),
+    },
+  })
 
-    socket.on('connect', () => {
-        console.log('Sockets Online');
-    });
+  socket.on('connect', () => {
+    console.log('Sockets Online')
+  })
 
-    socket.on('disconnect', () => {
-        console.log('Sockets Offline');
-    });
+  socket.on('disconnect', () => {
+    console.log('Sockets Offline')
+  })
 
-    socket.on('recibir-mensajes', mostrarMensajes);
+  socket.on('recibir-mensajes', mostrarMensajes)
 
-    socket.on('usuarios-activos', mostrarUsuarios);
+  socket.on('usuarios-activos', mostrarUsuarios)
 
-    // TODO: Crear nuevo metodo mostrarMensajesPrivados similar a mostrarMensajes:
-    // socket.on('mensaje-privado', mostrarMensajesPrivados);
-    // modificando la clase ChatMensajes para que soporte una nueva lista con los mensajes privados
-    socket.on('mensaje-privado', mostrarMensajes);
-};
-
+  // TODO: Crear nuevo metodo mostrarMensajesPrivados similar a mostrarMensajes:
+  // socket.on('mensaje-privado', mostrarMensajesPrivados);
+  // modificando la clase ChatMensajes para que soporte una nueva lista con los mensajes privados
+  socket.on('mensaje-privado', mostrarMensajes)
+}
 
 const mostrarUsuarios = (usuarios = []) => {
-    let usersHtml = '';
-    usuarios.forEach(({ nombre, uid }) => {
-        usersHtml += `
+  let usersHtml = ''
+  usuarios.forEach(({ nombre, uid }) => {
+    usersHtml += `
             <li>
                 <p>
                     <h5 class="text-success">${nombre}</h5>
                     <span class="fs-6 text-muted">${uid}</span>
                 </p>
             </li>
-        `;
-    });
+        `
+  })
 
-    ulUsuarios.innerHTML = usersHtml;
-};
+  ulUsuarios.innerHTML = usersHtml
+}
 
 const mostrarMensajes = (mensajes = []) => {
-    let mensajesHtml = '';
-    mensajes.forEach(({ mensaje, nombre }) => {
-        mensajesHtml += `
+  let mensajesHtml = ''
+  mensajes.forEach(({ mensaje, nombre }) => {
+    mensajesHtml += `
             <li>
                 <p>
                     <span class="text-primary">${nombre}: </span>
                     <span>${mensaje}</span>
                 </p>
             </li>
-        `;
-    });
-    ulMensajes.innerHTML = mensajesHtml;
-};
-
+        `
+  })
+  ulMensajes.innerHTML = mensajesHtml
+}
 
 // Agrego el evento keyup, que escucha cuando se presiona una tecla en el input
 // En el keyCode viene el numero ASCII de la tecla presionada
 txtMensaje.addEventListener('keyup', ({ keyCode }) => {
-    if (keyCode !== 13) { return };
+  if (keyCode !== 13) {
+    return
+  }
 
-    const mensaje = txtMensaje.value;
-    if (mensaje.length === 0) { return };
-    const uid = txtUid.value;
+  const mensaje = txtMensaje.value
+  if (mensaje.length === 0) {
+    return
+  }
+  const uid = txtUid.value
 
-    socket.emit('enviar-mensaje', { mensaje, uid });
+  socket.emit('enviar-mensaje', { mensaje, uid })
 
-    txtMensaje.value = '';
-});
-
+  txtMensaje.value = ''
+})
 
 const main = async () => {
-    // Validar JWT
-    await validarJWT();
-};
+  // Validar JWT
+  await validarJWT()
+}
 
-main();
-
-
+main()
